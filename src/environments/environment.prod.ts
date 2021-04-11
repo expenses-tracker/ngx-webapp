7723d81566ff7868a23e7a03b443e6d6ce277ab9
Varghese Abraham <varnit2010@gmail.com>
2021-04-11 12:15:23 +0530

    fix dev build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,7 +6,7 @@
 export const environment = {
   production: true,
   service: {
-    domain: process.env.SERVICE_DOMAIN,
+    domain: 'https://us-central1-com-varnit-expense-sheet.cloudfunctions.net/app',
     endpoints: {
       login: '/users/login',
       register: '/users/register',
