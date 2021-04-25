f7c5e04c936fba5e807f9a49ae74d66391bf195b
Varghese Abraham <varnit2010@gmail.com>
2021-04-25 11:15:09 +0530

    lint fixes

--- src/app/@core/services/http/httperrorinterceptor.service.ts
+++ src/app/@core/services/http/httperrorinterceptor.service.ts
@@ -4,7 +4,7 @@ import { catchError } from 'rxjs/operators';
 import { ToasterService, ToastType } from '../toaster.service';
 
 export class HttpErrorInterceptor implements HttpInterceptor {
-    constructor(private toast: ToasterService) {}
+  constructor(private toast: ToasterService) { }
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request)
       .pipe(
@@ -13,13 +13,12 @@ export class HttpErrorInterceptor implements HttpInterceptor {
           if (error.error instanceof ErrorEvent) {
             errorMsg = `Error: ${error.error.message}`;
             this.toast.showToast(ToastType.DANGER, 'Application Error', errorMsg);
-          }
-          else {
+          } else {
             errorMsg = `Error Code: ${error.status},  Message: ${error.message}`;
             this.toast.showToast(ToastType.DANGER, 'Service Error', errorMsg);
           }
           return throwError(errorMsg);
         })
-      )
+      );
   }
 }
